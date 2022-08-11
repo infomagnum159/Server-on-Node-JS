02381ce8a13978d29f51860864dc6a2c5262bc1a
infomagnum159 <infomagnum159@gmail.com>
2022-08-11 13:34:00 +0600

    Completed 2 task

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
 const express = require('express');
 const app = express();
 const port = 8000;
+const Vigenere = require('caesar-salad').Vigenere;
+const password = "password";
 
-app.get('/:name',  (req, res) => {
-    res.send( req.params.name)
+// app.get('/:name',  (req, res) => {
+//     res.send( req.params.name);
+// });
+
+
+app.get('/encode/:password', (req, res) => {
+    res.send(Vigenere.Cipher(password).crypt(req.params.password));
+});
+
+app.get('/decode/:password', (req, res) => {
+    res.send(Vigenere.Decipher(password).crypt(req.params.password));
 });
 
 app.listen(port, () => {
